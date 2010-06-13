77d0e7be5ba86ad667cdb69cb4706d179401c3d5
aberant <qzzzq1@gmail.com>
2010-06-13 09:37:54 -0500

    converting core_midi driver to use ffi and fixing snow leopard issue. inspired by evhan/tmdas project.

--- lib/midiator/drivers/core_midi.rb
+++ lib/midiator/drivers/core_midi.rb
@@ -15,35 +15,39 @@
 # This code released under the terms of the MIT license.
 #
 
-require 'dl/import'
+require 'ffi'
 
 require 'midiator'
 require 'midiator/driver'
 require 'midiator/driver_registry'
 
 class MIDIator::Driver::CoreMIDI < MIDIator::Driver # :nodoc:
+  SnowLeopard = `uname -r` =~ /10\.\d\.\d/
+
   ##########################################################################
   ### S Y S T E M   I N T E R F A C E
   ##########################################################################
-  module C # :nodoc:
-    extend DL::Importable
-    dlload '/System/Library/Frameworks/CoreMIDI.framework/Versions/Current/CoreMIDI'
-
-    extern "int MIDIClientCreate( void*, void*, void*, void* )"
-    extern "int MIDIClientDispose( void* )"
-    extern "int MIDIGetNumberOfDestinations()"
-    extern "void* MIDIGetDestination( int )"
-    extern "int MIDIOutputPortCreate( void*, void*, void* )"
-    extern "void* MIDIPacketListInit( void* )"
-    extern "void* MIDIPacketListAdd( void*, int, void*, int, int, int, void* )"
-    extern "int MIDISend( void*, void*, void* )"
+  module C
+    extend FFI::Library
+    ffi_lib '/System/Library/Frameworks/CoreMIDI.framework/Versions/Current/CoreMIDI'
+    attach_function :MIDIClientCreate, [:pointer, :pointer, :pointer, :pointer], :int
+    attach_function :MIDIClientDispose, [:pointer], :int
+    attach_function :MIDIGetNumberOfDestinations, [], :int
+    attach_function :MIDIGetDestination, [:int], :pointer
+    attach_function :MIDIOutputPortCreate, [:pointer, :pointer, :pointer], :int
+    attach_function :MIDIPacketListInit, [:pointer], :pointer
+    attach_function :MIDISend, [:pointer, :pointer, :pointer], :int
+    if SnowLeopard
+      attach_function :MIDIPacketListAdd, [:pointer, :int, :pointer, :int, :int, :pointer], :pointer
+    else
+      attach_function :MIDIPacketListAdd, [:pointer, :int, :pointer, :int, :int, :int, :pointer], :pointer
+    end
   end
 
-  module CF # :nodoc:
-    extend DL::Importable
-    dlload '/System/Library/Frameworks/CoreFoundation.framework/Versions/Current/CoreFoundation'
-
-    extern "void* CFStringCreateWithCString( void*, char*, int )"
+  module CF
+    extend FFI::Library
+    ffi_lib '/System/Library/Frameworks/CoreFoundation.framework/Versions/Current/CoreFoundation'
+    attach_function :CFStringCreateWithCString, [:pointer, :string, :int], :pointer
   end
 
   ##########################################################################
@@ -51,32 +55,41 @@ class MIDIator::Driver::CoreMIDI < MIDIator::Driver # :nodoc:
   ##########################################################################
 
   def open
-    client_name = CF.cFStringCreateWithCString( nil, "MIDIator", 0 )
-    @client = DL::PtrData.new( nil )
-    C.mIDIClientCreate( client_name, nil, nil, @client.ref )
+    client_name = CF.CFStringCreateWithCString( nil, "MIDIator", 0 )
 
-    port_name = CF.cFStringCreateWithCString( nil, "Output", 0 )
-    @outport = DL::PtrData.new( nil )
-    C.mIDIOutputPortCreate( @client, port_name, @outport.ref )
+    client_ptr = FFI::MemoryPointer.new :pointer
+    C.MIDIClientCreate client_name, nil, nil, client_ptr
+    @client = client_ptr.read_pointer
 
-    number_of_destinations = C.mIDIGetNumberOfDestinations
+    port_name = CF.CFStringCreateWithCString nil, "Output", 0
+    outport_ptr = FFI::MemoryPointer.new :pointer
+    C.MIDIOutputPortCreate @client, port_name, outport_ptr
+    @outport = outport_ptr.read_pointer
+
+    number_of_destinations = C.MIDIGetNumberOfDestinations
     raise MIDIator::NoMIDIDestinations if number_of_destinations < 1
-    @destination = C.mIDIGetDestination( 0 )
+    @destination = C.MIDIGetDestination( 0 )
   end
 
   def close
-    C.mIDIClientDispose( @client )
+    C.MIDIClientDispose( @client )
   end
 
   def message( *args )
     format = "C" * args.size
-    bytes = args.pack( format ).to_ptr
-    packet_list = DL.malloc( 256 )
-    packet_ptr = C.mIDIPacketListInit( packet_list )
+    bytes = FFI::MemoryPointer.new FFI.type_size(:char) * args.size
+    bytes.write_string args.pack(format)
 
-    # Pass in two 32-bit 0s for the 64 bit time
-    packet_ptr = C.mIDIPacketListAdd( packet_list, 256, packet_ptr, 0, 0, args.size, bytes )
+    packet_list = FFI::MemoryPointer.new 256
+    packet_ptr = C.MIDIPacketListInit packet_list
 
-    C.mIDISend( @outport, @destination, packet_list )
+    if SnowLeopard
+      packet_ptr = C.MIDIPacketListAdd packet_list, 256, packet_ptr, 0, args.size, bytes
+    else
+      # Pass in two 32-bit 0s for the 64 bit time
+      packet_ptr = C.MIDIPacketListAdd packet_list, 256, packet_ptr, 0, 0, args.size, bytes
+    end
+
+    C.MIDISend( @outport, @destination, packet_list )
   end
 end
