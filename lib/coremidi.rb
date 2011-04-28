a9990cd290ce8d86ca771c92bf8bc5363846b96d
Ari Russo <ari.russo@gmail.com>
2011-04-27 20:35:28 -0400

    copyright stuff

--- lib/coremidi.rb
+++ lib/coremidi.rb
@@ -1,6 +1,25 @@
 #!/usr/bin/env ruby
 #
-
-module CoreMIDIator
+# Realtime MIDI IO with Ruby for OSX using the coreMIDI framework.
+#
+# == Author
+#
+# * {Ari Russo}[http://github.com/arirusso] <ari.russo at gmail.com>
+#
+# == Credits
+#
+# This library began as a fork of the {MIDIator}[http://github.com/bleything/midiator] library
+#
+# The {fork}[http://github.com/aberant/midiator] that this library stemmed from, and a substantial amount code in this library was by
+#
+# * Colin Harris -- https://github.com/aberant
+#
+# and MIDIator itself was authored by
+#
+# * Ben Bleything -- ben@bleything.net
+# * Topher Cyll -- http://www.cyll.org
+#
+#
+module CoreMIDI
   VERSION = "0.0.1"
 end
