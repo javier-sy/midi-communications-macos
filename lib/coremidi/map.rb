ada0dd12acba77b483037d5afd99228da5beeb7c
Ari Russo <ari.russo@gmail.com>
2011-04-27 23:18:12 -0400

    map comments

--- lib/coremidi/map.rb
+++ lib/coremidi/map.rb
@@ -2,7 +2,7 @@
 module CoreMIDI
 
   #
-  # libasound RawMIDI struct, enum and function bindings
+  # CoreMIDI struct, enum and function bindings
   #
   module Map
 
@@ -24,15 +24,24 @@ module CoreMIDI
     end
 
     attach_function :MIDIClientCreate, [:pointer, :pointer, :pointer, :pointer], :int
+
     attach_function :MIDIClientDispose, [:pointer], :int
+
     attach_function :MIDIGetNumberOfDestinations, [], TypeAliases::ItemCount
+
     attach_function :MIDIGetNumberOfDevices, [], TypeAliases::ItemCount
+
     attach_function :MIDIGetDestination, [:int], :pointer
+
     attach_function :MIDIGetDevice, [TypeAliases::ItemCount], MIDIDeviceRef
+
     # OSStatus MIDIObjectGetStringProperty (MIDIObjectRef  obj, CFStringRef propertyID, CFStringRef *str);
     attach_function :MIDIObjectGetStringProperty, [TypeAliases::MIDIObjectRef, TypeAliases::CFStringRef, :pointer], TypeAliases::OSStatus
+
     attach_function :MIDIOutputPortCreate, [:pointer, :pointer, :pointer], :int
+
     attach_function :MIDIPacketListInit, [:pointer], :pointer
+
     attach_function :MIDISend, [:pointer, :pointer, :pointer], :int
 
     if SnowLeopard
@@ -42,10 +51,15 @@ module CoreMIDI
     end
 
     module CF
+
       extend FFI::Library
       ffi_lib '/System/Library/Frameworks/CoreFoundation.framework/Versions/Current/CoreFoundation'
+
+      # CFString* CFStringCreateWithCString( ?, CString, encoding)
       attach_function :CFStringCreateWithCString, [:pointer, :string, :int], :pointer
+      # CString* CFStringGetCStringPtr(CFString*, encoding)
       attach_function :CFStringGetCStringPtr, [:pointer, :int], :pointer
+
     end
 
   end
