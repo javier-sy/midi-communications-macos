bfc16de0967d31b902c49cf17e6369b88867f5db
Ari Russo <ari.russo@gmail.com>
2011-04-29 20:19:31 -0400

    sysex output

--- lib/coremidi/map.rb
+++ lib/coremidi/map.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
+
 module CoreMIDI
 
   #
   # coremidi struct, enum and function bindings
+  #
+  # Some comments from from "Mac OS X Developer Library", Apple Computer, Inc.
+  # https://developer.apple.com
   #
   module Map
 
@@ -21,9 +25,24 @@ module CoreMIDI
       OSStatus = :int
     end
 
-    class MIDIDeviceRef < FFI::Struct
-      include TypeAliases
+    callback :sysex_output_callback, [:pointer], :void
 
+    # This represents a request to send a single system-exclusive MIDI event to a MIDI destination asynchronously.
+    class MIDISysexSendRequest < FFI::Struct
+
+             # The endpoint to which the event is to be sent.
+      layout :destination,         TypeAliases::MIDIEndpointRef,
+             # Initially, a pointer to the sys-ex event to be sent.  MIDISendSysex will advance this pointer as bytes are sent.
+             :data,                :pointer,
+             # Initially, the number of bytes to be sent. MIDISendSysex will decrement this counter as bytes are sent.
+             :bytes_to_send,       :uint32,
+             # The client may set this to true at any time to abort transmission. The implementation sets this to true when all bytes have been sent.
+             :complete,            :int,
+             :reserved,            [:char, 3],
+             # Called when all bytes have been sent, or after the client has set complete to true.
+             :completion_proc,     :sysex_output_callback,
+             # Passed as a refCon to completionProc.
+             :completion_ref_con,  :pointer
     end
 
     attach_function :MIDIClientCreate, [:pointer, :pointer, :pointer, :pointer], :int
@@ -51,7 +70,7 @@ module CoreMIDI
     # MIDIEndpointRef MIDIEntityGetSource (MIDIEntityRef  entity, ItemCount sourceIndex0);
     attach_function :MIDIEntityGetSource, [TypeAliases::MIDIEntityRef, TypeAliases::ItemCount], TypeAliases::MIDIEndpointRef
 
-    attach_function :MIDIGetDevice, [TypeAliases::ItemCount], MIDIDeviceRef
+    attach_function :MIDIGetDevice, [TypeAliases::ItemCount], TypeAliases::MIDIDeviceRef
 
     # OSStatus MIDIObjectGetStringProperty (MIDIObjectRef  obj, CFStringRef propertyID, CFStringRef *str);
     attach_function :MIDIObjectGetStringProperty, [TypeAliases::MIDIObjectRef, TypeAliases::CFStringRef, :pointer], TypeAliases::OSStatus
@@ -61,6 +80,8 @@ module CoreMIDI
     attach_function :MIDIPacketListInit, [:pointer], :pointer
 
     attach_function :MIDISend, [:pointer, :pointer, :pointer], :int
+
+    attach_function :MIDISendSysex, [:pointer], :int
 
     if SnowLeopard
       attach_function :MIDIPacketListAdd, [:pointer, :int, :pointer, :int, :int, :pointer], :pointer
--- lib/coremidi/output.rb
+++ lib/coremidi/output.rb
@@ -25,17 +25,11 @@ module CoreMIDI
       bytes = (FFI::MemoryPointer.new FFI.type_size(:char) * data.size)
       bytes.write_string(data.pack(format))
 
-      packet_list = FFI::MemoryPointer.new(256)
-      packet_ptr = Map.MIDIPacketListInit(packet_list)
-
-      if Map::SnowLeopard
-        packet_ptr = Map.MIDIPacketListAdd(packet_list, 256, packet_ptr, 0, data.size, bytes)
+      if data.first.eql?(0xF0) && data.last.eql?(0xF7)
+        puts_sysex(bytes, data.size)
       else
-        # Pass in two 32-bit 0s for the 64 bit time
-        packet_ptr = Map.MIDIPacketListAdd(packet_list, 256, packet_ptr, 0, 0, data.size, bytes)
+        puts_small(bytes, data.size)
       end
-
-      Map.MIDISend( @outport, @destination, packet_list )
     end
 
     # send a MIDI message of an indeterminant type
@@ -85,6 +79,44 @@ module CoreMIDI
     def self.all
       Entity.all_by_type[:output]
     end
+
+    private
+
+    def puts_small(bytes, size)
+      packet_list = FFI::MemoryPointer.new(256)
+      packet_ptr = Map.MIDIPacketListInit(packet_list)
+
+      if Map::SnowLeopard
+        packet_ptr = Map.MIDIPacketListAdd(packet_list, 256, packet_ptr, 0, size, bytes)
+      else
+        # Pass in two 32-bit 0s for the 64 bit time
+        packet_ptr = Map.MIDIPacketListAdd(packet_list, 256, packet_ptr, 0, 0, size, bytes)
+      end
+
+      Map.MIDISend( @outport, @destination, packet_list )
+    end
+
+    def puts_sysex(bytes, size)
+
+      @callback =
+
+      request = Map::MIDISysexSendRequest.new
+      request[:destination] = @destination
+      request[:data] = bytes
+      request[:bytes_to_send] = size
+      request[:complete] = 0
+      request[:completion_proc] = SysexCompletionCallback
+      request[:completion_ref_con] = request
+
+      Map.MIDISendSysex(request)
+    end
+
+    SysexCompletionCallback =
+      Proc.new do |sysex_request_ptr|
+        # this isn't working for some reason
+        # as of now, we don't need it though
+      end
+
   end
 
 end
