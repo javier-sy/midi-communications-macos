e2c421718c5996182d1858e400e8e2e74422735c
Ari Russo <ari.russo@gmail.com>
2011-05-12 20:28:19 -0400

    gem version, credit

--- lib/coremidi.rb
+++ lib/coremidi.rb
@@ -1,13 +1,7 @@
 #!/usr/bin/env ruby
 #
-# This library began with some coremidi/ffi binding code for MIDI output by
-#
-# * Colin Harris -- http://github.com/aberant
-#
-# contained in {his fork of MIDIator}[http://github.com/aberant/midiator] and a {blog post}[http://aberant.tumblr.com/post/694878119/sending-midi-sysex-with-core-midi-and-ruby-ffi]
-#
-# {MIDIator}[http://github.com/bleything/midiator] is (c)2008 by Ben Bleything and Topher Cyll and released under the MIT license (see LICENSE.midiator and LICENSE.prp)
-#
+# (c)2011 Ari Russo
+# 
 require 'ffi'
 
 require 'coremidi/device'
@@ -17,5 +11,5 @@ require 'coremidi/map'
 require 'coremidi/output'
 
 module CoreMIDI
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
