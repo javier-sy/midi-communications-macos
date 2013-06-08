a70cacd54c0cf1a22d45431c6670a69154153fb8
Ari Russo <ari.russo@gmail.com>
2013-06-08 11:03:04 -0400

    extract Device#populate_name, comments

--- lib/coremidi/device.rb
+++ lib/coremidi/device.rb
@@ -1,46 +1,44 @@
-#!/usr/bin/env ruby
-
 module CoreMIDI
 
   class Device
 
     attr_reader :entities,
-                # unique Numeric id
-                :id,
-                # device name from coremidi
-                :name
+                :id, # Unique Numeric id
+                :name # Device name from coremidi
 
     def initialize(id, device_pointer, options = {})
-      include_if_offline = options[:include_offline] || false
+      include_if_offline = options.fetch(:include_offline, false)
       @id = id
       @resource = device_pointer
       @entities = []
-      
-      prop = Map::CF.CFStringCreateWithCString( nil, "name", 0 )
-
-      begin
-        name_ptr = FFI::MemoryPointer.new(:pointer)
-        Map::MIDIObjectGetStringProperty(@resource, prop, name_ptr)
-        name = name_ptr.read_pointer
-        len = Map::CF.CFStringGetMaximumSizeForEncoding(Map::CF.CFStringGetLength(name), :kCFStringEncodingUTF8)
-        bytes = FFI::MemoryPointer.new(len + 1)
-        raise RuntimeError.new("CFStringGetCString") unless Map::CF.CFStringGetCString(name, bytes, len, :kCFStringEncodingUTF8)
-        @name = bytes.read_string
-      ensure
-        Map::CF.CFRelease(name) unless name.nil? || name.null?
-        Map::CF.CFRelease(prop) unless prop.null?
-      end
-
+      populate_name
       populate_entities(:include_offline => include_if_offline)
     end
+            
+    # Endpoints for this device
+    # @return [Array<Endpoint>]
+    def endpoints
+      endpoints = { :source => [], :destination => [] }
+      endpoints.keys.each do |k|
+        endpoints[k] += entities.map { |entity| entity.endpoints[k] }.flatten
+      end
+      endpoints
+    end
     
-    # returns all devices which are cached in an instance variable @devices on the Device class
-    #
-    # options:
-    # 
-    # * <b>cache</b>: if false, the device list will never be cached. this would be useful if one needs to alter the device list (e.g. plug in a USB MIDI interface) while their program is running.
-    # * <b>include_offline</b>: if true, devices marked offline by coremidi will be included in the list
-    #
+    # Assign all of this Device's endpoints an consecutive local id
+    # @param [Integer] last_id The highest already used endpoint ID 
+    # @return [Integer] The highest used endpoint ID after populating this device's endpoints
+    def populate_endpoint_ids(last_id)
+      i = 0
+      entities.each { |entity| i += entity.populate_endpoint_ids(i + last_id) }
+      i
+    end
+
+    # All cached devices
+    # @param [Hash] options The options to select devices with
+    # @option options [Boolean] :cache If false, the device list will never be cached. This would be useful if one needs to alter the device list (e.g. plug in a USB MIDI interface) while their program is running.
+    # @option options [Boolean] :include_offline If true, devices marked offline by coremidi will be included in the list
+    # @return [Array<Device>] All cached devices
     def self.all(options = {})
       use_cache = options[:cache] || true
       include_offline = options[:include_offline] || false
@@ -56,38 +54,42 @@ module CoreMIDI
       end
       @devices
     end
-    
-    # Refresh the Device cash.  You'll need to do this if, for instance, you plug in
-    # a USB MIDI device while the program is running 
+
+    # Refresh the Device cache. This is needed if, for example a USB MIDI device is plugged in while the program is running
+    # @return [Array<Device>] The Device cache
     def self.refresh
       @devices.clear
-    end
-    
-    # returns all of the Endpoints for this device
-    def endpoints
-      endpoints = { :source => [], :destination => [] }
-      endpoints.keys.each do |k|
-        endpoints[k] += entities.map { |entity| entity.endpoints[k] }.flatten
-      end
-      endpoints
-    end
-    
-    # assign all of this Device's endpoints an consecutive local id
-    def populate_endpoint_ids(starting_id)
-      i = 0
-      entities.each { |entity| i += entity.populate_endpoint_ids(i + starting_id) }
-      i
+      @devices
     end
 
     private
+
+    # Populate the device name
+    def populate_name
+      prop = Map::CF.CFStringCreateWithCString( nil, "name", 0 )
+
+      begin
+        name_ptr = FFI::MemoryPointer.new(:pointer)
+        Map::MIDIObjectGetStringProperty(@resource, prop, name_ptr)
+        name = name_ptr.read_pointer
+        len = Map::CF.CFStringGetMaximumSizeForEncoding(Map::CF.CFStringGetLength(name), :kCFStringEncodingUTF8)
+        bytes = FFI::MemoryPointer.new(len + 1)
+        raise RuntimeError.new("CFStringGetCString") unless Map::CF.CFStringGetCString(name, bytes, len, :kCFStringEncodingUTF8)
+        @name = bytes.read_string
+      ensure
+        Map::CF.CFRelease(name) unless name.nil? || name.null?
+        Map::CF.CFRelease(prop) unless prop.null?
+      end
+    end
     
-    # gives all of the endpoints for all devices a consecutive local id
+    # All of the endpoints for all devices a consecutive local id
     def self.populate_endpoint_ids
       i = 0
       all.each { |device| i += device.populate_endpoint_ids(i) }
     end
 
-    # populates the entities for this device. these are in turn used to gather the endpoints
+    # Populates the entities for this device. These entities are in turn used to gather the endpoints.
+    #
     def populate_entities(options = {})
       include_if_offline = options[:include_offline] || false
       i = 0
