359fe6b7e1d09bc84333c13e4b045d01fcc7f154
Ari Russo <ari.russo@gmail.com>
2014-09-01 23:33:39 -0400

    use contexts

--- test/input_buffer_test.rb
+++ test/input_buffer_test.rb
@@ -2,42 +2,41 @@ require "helper"
 
 class InputBufferTest < Test::Unit::TestCase
 
-  include CoreMIDI
-  include TestHelper
+  context "CoreMIDI" do
 
-  def test_input_buffer
-    sleep(1)
+    setup do
+      sleep(1)
+    end
 
-    messages = VariousMIDIMessages
-    bytes = []
+    context "Source#buffer" do
 
-    $test_device[:output].open do |output|
-      $test_device[:input].open do |input|
-        
-        input.buffer.clear
+      setup do
+        @messages = TestHelper::VariousMIDIMessages
+        @messages_arr = @messages.inject { |a,b| a+b }.flatten
+        @received_arr = []
+        @pointer = 0
 
-        messages.each do |msg|
+        @output = $test_device[:output].open
+        @input = $test_device[:input].open
+        @input.buffer.clear
+      end
 
-          $>.puts "sending: " + msg.inspect
+      should "have the correct messages in the buffer" do
+        bytes = []
+        @messages.each do |message|
+          puts "sending: #{message.inspect}"
+          @output.puts(message)
+          bytes += message 
 
-          output.puts(msg)
-          
-          bytes += msg 
-          
           sleep(0.5)
-          
-          buffer = input.buffer.map { |m| m[:data] }.flatten
 
-          $>.puts "received: " + buffer.to_s
-          
+          buffer = @input.buffer.map { |m| m[:data] }.flatten
+          puts "received: #{buffer.to_s}"
           assert_equal(bytes, buffer)
-
         end
-        
-        assert_equal(bytes.length, input.buffer.map { |m| m[:data] }.flatten.length)
-
+        assert_equal(bytes.length, @input.buffer.map { |m| m[:data] }.flatten.length)
       end
     end
-  end
 
+  end
 end
